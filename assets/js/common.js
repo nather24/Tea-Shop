ff58b28b5c4834d5a1e8dbfb0a5aa8b1df8ee54d
Dinesh K <arasudinesh230@gmail.com>
2026-01-30 23:19:46 +0530

    Refactor navbar scroll effect and add quick search
    Refactor scroll event listener and add quick search function.

--- assets/js/common.js
+++ assets/js/common.js
@@ -2,12 +2,20 @@
 function toggleMenu(){
   document.getElementById("navLinks").classList.toggle("active");
 }
+
 /* Navbar scroll effect */
-window.addEventListener("scroll", function(){
+window.addEventListener("scroll", function () {
   var navbar = document.querySelector(".navbar");
-  if(window.scrollY > 30){
+
+  if (window.scrollY > 30) {
     navbar.classList.add("scrolled");
-  }else{
+  } else {
     navbar.classList.remove("scrolled");
   }
 });
+
+/* Quick search redirect */
+function quickSearch(place) {
+  window.location.href = "search.html?q=" + encodeURIComponent(place);
+}
+
